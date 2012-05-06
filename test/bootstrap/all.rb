998713458963bd03b57c9f2cbb5fefebbc78ab89
David Barri <japgolly@gmail.com>
2012-05-06 11:15:15 +1000

    Oops, removing local test modification.

--- test/bootstrap/all.rb
+++ test/bootstrap/all.rb
@@ -1,7 +1,4 @@
 # encoding: utf-8
 APP_ROOT = File.expand_path('../../..',__FILE__)
-
-require '/home/golly/projects/corvid/lib/corvid/test/simplecov.rb'
-
 require 'rubygems'
 require 'corvid/test/bootstrap/all'
