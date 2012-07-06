940491e28c1d79019e8d358aea423d226ce70d52
David Barri <japgolly@gmail.com>
2012-07-06 13:04:56 +1000

    One of the extremely rare times where I copy&paste. This project now has its own copy of env_helpers. Reason: corvid is not a runtime dep for this and I want that to stay that way.

--- lib/golly-utils/ruby_ext/env_helpers.rb
+++ lib/golly-utils/ruby_ext/env_helpers.rb
@@ -1,3 +1,27 @@
-# This has been moved to Corvid.
-# Logically it should be here but this is a Corvid project and I don't want to create a cyclic dependency.
-require 'corvid/env_helpers'
+module GollyUtils
+  module EnvHelpers
+
+    def boolean(key, default=nil)
+      return default unless self.has_key?(key)
+      v= self[key]
+      return true if v =~ /^\s*(?:[1yt]|yes|true|on|enabled?)\s*$/i
+      return false if v =~ /^\s*(?:[0nf]|no|false|off|disabled?)\s*$/i
+      STDERR.puts "Unable to parse boolean value #{v.inspect} for key #{key.inspect}."
+      default
+    end
+
+    alias yes? boolean
+    alias on? boolean
+    alias enabled? boolean
+
+    def no?(key, default=nil)
+      !boolean(key, default)
+    end
+    alias off? no?
+    alias disabled? no?
+
+  end
+end
+
+# Add helpers to ENV
+ENV.send :extend, GollyUtils::EnvHelpers
