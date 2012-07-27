8aa46a7118491c34eea240a0b0f5814c7bd0262e
David Barri <japgolly@gmail.com>
2012-07-28 00:30:29 +1000

    Added test helpers for file-related testing

---
+++ lib/golly-utils/test/spec/files.rb
@@ -0,0 +1,114 @@
+require 'golly-utils/test/helpers/files'
+
+module GollyUtils
+  module TestHelpers
+    module ClassMethods
+
+      def run_each_in_empty_dir
+        eval <<-EOB
+          around :each do |ex|
+            inside_empty_dir{ ex.run }
+          end
+        EOB
+      end
+
+      def run_all_in_empty_dir(&block)
+        block ||= Proc.new{}
+        @@around_all_in_empty_dir_count ||= 0
+        @@around_all_in_empty_dir_count += 1
+        block_name= :"@@around_all_in_empty_dir_#@@around_all_in_empty_dir_count"
+        ::GollyUtils::TestHelpers::ClassMethods.class_variable_set block_name, block
+        eval <<-EOB
+          before(:all){
+            @old_dir,@tmp_dir = inside_empty_dir
+            block= ::GollyUtils::TestHelpers::ClassMethods.class_variable_get(:"#{block_name}")
+            instance_exec &block
+          }
+          after(:all){ step_out_of_tmp_dir }
+        EOB
+      end
+
+    end
+
+    module RSpecMatchers
+
+      #-----------------------------------------------------------------------------------------------------------------
+
+      class ExistAsFile
+        def matches?(tgt)
+          @tgt= tgt
+          File.exists? tgt and File.file? tgt
+        end
+
+        def failure_message_for_should
+          m "expected that '#@tgt' would be an existing file."
+        end
+
+        def failure_message_for_should_not
+          m "expected that '#@tgt' would not exist."
+        end
+
+        private
+        def m(msg)
+          dir= File.dirname(@tgt)
+          if Dir.exists? dir
+            Dir.chdir(dir) {
+              files= Dir.glob('*',File::FNM_DOTMATCH).select{|f| File.file? f }.sort
+              indir= dir == '.' ? '' : " in #{dir}"
+              "#{msg}\nFiles found#{indir}: #{files.join '  '}"
+            }
+          else
+            "#{msg}\nDirectory doesn't exist: #{dir}"
+          end
+        end
+      end
+
+      def exist_as_a_file
+        ExistAsFile.new
+      end
+      alias :exist_as_file :exist_as_a_file
+
+      #-----------------------------------------------------------------------------------------------------------------
+
+      class ExistAsDir
+        def matches?(tgt)
+          @tgt= tgt
+          Dir.exists? tgt
+        end
+
+        def failure_message_for_should
+          m "expected that '#@tgt' would be an existing directory."
+        end
+
+        def failure_message_for_should_not
+          m "expected that '#@tgt' would not exist."
+        end
+
+        private
+        def m(msg)
+          dir= File.expand_path('..',@tgt).sub(Dir.pwd+'/','')
+          if Dir.exists? dir
+            Dir.chdir(dir) {
+              dirs= Dir.glob('*',File::FNM_DOTMATCH).select{|f| File.directory? f }.sort
+              indir= dir == '.' ? '' : " in #{dir}"
+              "#{msg}\nDirs found#{indir}: #{dirs.join '  '}"
+            }
+          else
+            "#{msg}\nDirectory doesn't exist: #{dir}"
+          end
+        end
+      end
+
+      def exist_as_a_dir
+        ExistAsDir.new
+      end
+      alias :exist_as_dir :exist_as_a_dir
+
+    end
+  end
+end
+
+RSpec::configure do |config|
+  config.include GollyUtils::TestHelpers
+  config.include GollyUtils::TestHelpers::RSpecMatchers
+end
---
+++ test/spec/test/spec/files_spec.rb
@@ -0,0 +1,94 @@
+# encoding: utf-8
+require_relative '../../../bootstrap/spec'
+require 'golly-utils/test/spec/files'
+
+TMP_TEST_FILE= '.rubbish.tmp'
+
+describe 'RSpec helpers' do
+  context 'run_each_in_empty_dir' do
+    run_each_in_empty_dir
+    def test
+      get_files.should be_empty
+      File.write 'hehe', 'hehe'
+      get_files.should_not be_empty
+    end
+    it("should provide each example with an empty directory (1/2)"){ test }
+    it("should provide each example with an empty directory (2/2)"){ test }
+  end
+
+  context 'run_all_in_empty_dir' do
+    context 'without block' do
+      run_all_in_empty_dir
+      def test
+        if $run_all_in_empty_dir__no_block.nil?
+          File.write 'hehe1', 'hehe'
+          $run_all_in_empty_dir__no_block= true
+        end
+        get_files.should == %w[hehe1]
+      end
+      it("should provide and reuse an empty directory for all examples (1/2)"){ test }
+      it("should provide and reuse an empty directory for all examples (2/2)"){ test }
+    end
+
+    context 'with block' do
+      run_all_in_empty_dir do
+        File.write 'hehe1', 'hehe'
+      end
+      def test
+        if $run_all_in_empty_dir__block.nil?
+          File.write 'hehe2', 'hehe'
+          $run_all_in_empty_dir__block= true
+        end
+        get_files.should == %w[hehe1 hehe2]
+      end
+      it("should provide and reuse an empty directory for all examples (1/2)"){ test }
+      it("should provide and reuse an empty directory for all examples (2/2)"){ test }
+    end
+  end
+end
+
+describe 'RSpec matchers' do
+  context 'exist_as_file' do
+    run_each_in_empty_dir
+
+    it("should pass when a file exists"){
+      File.write TMP_TEST_FILE, 'blah'
+      TMP_TEST_FILE.should exist_as_file
+    }
+
+    it("should fail when a file doesnt exist"){
+      expect{
+        TMP_TEST_FILE.should exist_as_file
+      }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
+    }
+
+    it("should fail when a file is a directory"){
+      Dir.mkdir TMP_TEST_FILE
+      expect{
+        TMP_TEST_FILE.should exist_as_file
+      }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
+    }
+  end
+
+  context 'exist_as_dir' do
+    run_each_in_empty_dir
+
+    it("should pass when a dir exists"){
+      Dir.mkdir TMP_TEST_FILE
+      TMP_TEST_FILE.should exist_as_dir
+    }
+
+    it("should fail when a dir doesnt exist"){
+      expect{
+        TMP_TEST_FILE.should exist_as_dir
+      }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
+    }
+
+    it("should fail when a dir is a file"){
+      File.write TMP_TEST_FILE, 'blah'
+      expect{
+        TMP_TEST_FILE.should exist_as_dir
+      }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
+    }
+  end
+end
