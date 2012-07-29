ee19b30315a18a8b2bb4705a88224c73aa6c2fd9
David Barri <japgolly@gmail.com>
2012-07-29 23:03:31 +1000

    Doco

--- lib/golly-utils/ruby_ext/pretty_error_messages.rb
+++ lib/golly-utils/ruby_ext/pretty_error_messages.rb
@@ -2,7 +2,7 @@ class StandardError
 
   # Creates a string that provides more information (a backtrace) than the default `to_s` method.
   #
-  # @return String
+  # @return [String]
   def to_pretty_s
     "#{self.inspect}\n#{self.backtrace.map{|b| "  #{b}"}.join "\n"}"
   end
